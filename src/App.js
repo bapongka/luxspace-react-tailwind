c5a5719bebb5f0a233823c38bb70230a034f88b1
Ridho Rakhaman <91518402+bapongka@users.noreply.github.com>
2022-04-12 23:38:07 +0700

    folder structuring
    update src

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+// import './App.css';
+import "./assets/css/app.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="">halo ridhou</h1>
+    </div>
+  );
+}
+
+export default App;
