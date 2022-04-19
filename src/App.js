8317d8ed3a0eb2922ab3915ddb1c41e03d9c1322
Ridho Rakhaman <91518402+bapongka@users.noreply.github.com>
2022-04-19 14:49:47 +0700

    Add file
    Pages
    -Notfound
    Parts
    -PageError

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import HomePage from "./pages/HomePage";
 import Details from "./pages/Details";
 import Cart from "./pages/Cart";
 import Congratulation from "./pages/Congratulation";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/categories/:idc" element={<Details />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/congratulation" element={<Congratulation />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
